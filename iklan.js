329a4145b2b69ef7a3148c3be708d3e9c0e9a514
can354dra <candradwiputro@gmail.com>
2018-11-21 18:43:30 +0700

    Delete iklan.js

--- iklan.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-  ( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"can354dra","width":300,"height":250,"sid":"Chitika Default"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
-</script>
